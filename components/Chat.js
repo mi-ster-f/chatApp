a0a0bc7bb9cf4fa4386c87b5434f2ae8fb3604cd
fratzio <thomasmichaelfrazer@gmail.com>
2020-08-04 23:37:50 -0700

    Added GiftedChat library to project and styled Bubble + SystemMessage props for Chat.js. Also added keyboard spacer to fix android keyboard behavior

--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,68 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { GiftedChat, Bubble, SystemMessage } from 'react-native-gifted-chat';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 
 // The application’s main Chat component that renders the chat UI
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { messages: [] };
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 3,
+          text: 'This is a system message',
+          createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  // function prop for GiftedChat to customise the chat bubble
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000',
+          },
+        }}
+      />
+    );
+  }
+
+  // Send a system message to chat initially
+  renderSystemMessage(props) {
+    return (
+      <SystemMessage
+        {...props}
+        containerStyle={{ backgroundColor: 'pink' }}
+        wrapperStyle={{ borderWidth: 10, borderColor: 'white' }}
+        textStyle={{ color: 'crimson', fontWeight: '900' }}
+      />
+    );
   }
 
   render() {
@@ -13,15 +71,17 @@ export default class Chat extends React.Component {
     let chosenColor = this.props.route.params.color;
 
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: chosenColor,
-        }}
-      >
-        <Text>Hello Screen2!</Text>
+      <View style={{ flex: 1 }}>
+        <GiftedChat
+          renderBubble={this.renderBubble}
+          renderSystemMessage={this.renderSystemMessage}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardSpacer /> : null}
       </View>
     );
   }
